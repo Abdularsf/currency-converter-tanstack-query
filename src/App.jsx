96867d8e8c5822f932fd2449d33ca129bb45482d
Abdul Rahman <abdularsf147@gmail.com>
2026-02-20 19:36:22 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,47 @@
 const App = () => {
-  return <h1>currency converter</h1>
+  return (
+    <section className="currency-converter">
+      <div className="currency-div">
+        <h1>Currency Converter</h1>
+        <hr />
+        <div>
+          <label>
+            Amount:
+            <input type="text" placeholder="Amount" />
+          </label>
+        </div>
+        <section className="currency-selector">
+          <label>
+            From:
+            <select>
+              {
+                ["USD", "EUR", "GBP", "INR", "AUD"].map((currency) => {
+                  return (
+                    <option key={currency} value={currency}>
+                      {currency}
+                    </option>
+                  )
+                })
+              }
+            </select>
+          </label>
+          <label>
+            To:
+            <select>
+              {
+                ["INR", "USD", "EUR", "GBP", "AUD"].map((currency) => {
+                  return (
+                    <option key={currency} value={currency}>
+                      {currency}
+                    </option>
+                  )
+                })
+              }
+            </select>
+          </label>
+        </section>
+      </div>
+    </section>
+  )
 }
 export default App
